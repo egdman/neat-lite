cb2d3bd4e6d11db372e0a7b9a1fb04785235a0dd
Dmitry Egorov <egdman90@gmail.com>
2022-06-01 01:15:25 +0200

    set the version to 0.4.0

--- neat/__init__.py
+++ neat/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'Dmitry Egorov'
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 
 from .specs import GeneSpec, ParamSpec, bounds, gen, mut
 from .genes import NeuronGene, ConnectionGene, Genome
