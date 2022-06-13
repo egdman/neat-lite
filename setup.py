f424c1aa67d6bfc8fe931cffc022e30f9850758e
Dmitry Egorov <egdman90@gmail.com>
2022-06-13 15:48:41 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 from neat import __version__
 
 setup(
 	name='neat-lite',
 	version=__version__,
-	description='A lightweight library that implements NeuroEvolution through Augmenting Topologies.',
-	url='https://github.com/egdman/neat',
+	description='A dependency-free Python 3 library that implements NeuroEvolution through Augmenting Topologies.',
+	url='https://github.com/egdman/neat-lite',
 	author='Dmitry Egorov',
 	author_email='egdman90@gmail.com',
 	classifiers = [
 		'Development Status :: 2 - Pre-Alpha',
 		'Intended Audience :: Science/Research',
 		'License :: OSI Approved :: MIT License',
-		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3.7',
 		'Topic :: Scientific/Engineering :: Artificial Intelligence'
 	],
 	license='MIT',
 	packages=['neat'],
-	install_requires=['numpy']
-
 )
